153f9935d4a51d24b44c8f912330b588aadeb79d
karim-moftah <66466614+karim-moftah@users.noreply.github.com>
2022-05-21 08:08:15 +0200

    Update PORTF.c

--- Drivers/PORTF.c
+++ Drivers/PORTF.c
@@ -6,11 +6,11 @@ void PORTF_Init(void){  unsigned long volatile delay;
   delay = SYSCTL_RCGC2_R;
   GPIO_PORTF_LOCK_R = 0x4C4F434B; // unlock GPIO Port F
   GPIO_PORTF_CR_R = 0x11;           // allow changes to PF4-0  0001 0001
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output      changed from 6E to led1,2
-  GPIO_PORTF_AFSEL_R &= ~0x11;  //     disable alt funct on PF4
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0    6E from 1F       
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output      
+  GPIO_PORTF_AFSEL_R &= ~0x11;  //     disable alt funct 
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0          
   GPIO_PORTF_PCTL_R &= ~0x000F000F; //  configure PF4 as GPIO
-  GPIO_PORTF_AMSEL_R &= ~0x11;  //     disable analog functionality on PF4
+  GPIO_PORTF_AMSEL_R &= ~0x11;  //     disable analog functionality 
   GPIO_PORTF_PUR_R |= 0x11;     //     enable weak pull-up on PF4
   GPIO_PORTF_IS_R &= ~0x11;     // (d) PF4,PF0 is edge-sensitive
   GPIO_PORTF_IBE_R &= ~0x11;    //     PF4,PF0 is not both edges
